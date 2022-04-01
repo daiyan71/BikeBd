552bf328c67d56034e2a2aef8feaf5e6204e5b3c
DAIYAN IBRAHIM <46761584+daiyan71@users.noreply.github.com>
2022-04-01 17:04:48 +0600

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,115 @@
+<?php
+
+	session_start();
+	
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include 'link.php'?>   
+	    <link rel="stylesheet" href="aboutus.css">
+		 
+	</head>
+
+	<body>
+
+	<?php include 'navbar.php'?>
+
+	<div class="mainn">
+		
+		<div class="bgimg">
+			
+			<div class="container text-center headerset">
+				<h1>About Us</h1>
+				<h3>Buy Bike</h3>	
+			</div>
+			
+		</div>
+
+		<div class="container" style="margin-bottom: 0px;">
+			<div  class='row'>
+				<div class="col-md-6" style="margin-top: 00px; margin-bottom: 100px; padding:0px 40px;">
+						
+						
+						
+						
+					<div class='box1'>
+						<h2 class="text1">Daiyan Ibrahim</h2>
+						<p class="text2">Ahsanullah University of Science and Technology<br>Department: CSE<br>Semester: 3.1<br>Section: C2<br>ID: 17.01.04.144</p>
+						<h5 class="font-weight-bold text-uppercase mb-6"style="color: #ffc107; text-align: center;">Follow Us On</h5>
+
+							    <div style="margin: auto; width: 130px;">
+						        <a href="https://www.facebook.com/daiyan.ibrahim71"><img src="img/fb.jpg" style="height: 40px;width: 40px; margin: auto;"></a>
+
+						        <a href="https://twitter.com/iamsrk?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><img src="img/twitter.jpg" style="height: 40px;width: 40px;  "></a>
+						        <a href="https://www.instagram.com/Daiyan_ibrahim/?fbclid=IwAR21ockl3IzergeB2-jCtsggutY1JExVcy-gAPlMX4oCISuLCY11vLGB0YQ"><img src="img/insta.jpg" style="height: 40px;width: 40px; "></a>
+						    </div>
+
+					</div>
+					<div class="lock">
+							<img style="border-radius: 50%;"src="img/daiyan.jpg"/>
+						</div>
+				</div>
+				<div class="col-md-6" style="margin-top: 00px; margin-bottom: 100px; padding:0px 40px;"> 
+						
+						
+						
+					<div class='box2'>
+						<h2 class="text1">Sajat Nihal</h2>
+						<p class="text2">Ahsanullah University of Science and Technology<br>Department: CSE<br>Semester: 3.1<br>Section: C2<br>ID: 17.01.04.145</p>
+
+
+
+							    <h5 class="font-weight-bold text-uppercase mb-6"style="color: #ffc107; text-align: center;">Follow Us On</h5>
+
+							    <div style="margin: auto; width: 130px;">
+						        <a href="https://www.facebook.com/profile.php?id=100010225087847"><img src="img/fb.jpg" style="height: 40px;width: 40px; margin: auto;"></a>
+
+						        <a href="https://twitter.com/iamsrk?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><img src="img/twitter.jpg" style="height: 40px;width: 40px;  "></a>
+						        <a href="https://www.instagram.com/Daiyan_ibrahim/?fbclid=IwAR21ockl3IzergeB2-jCtsggutY1JExVcy-gAPlMX4oCISuLCY11vLGB0YQ"><img src="img/insta.jpg" style="height: 40px;width: 40px; "></a>
+						    </div>
+
+
+					</div>
+					<div class="cloud">
+							<img style="border-radius: 50%;" src="img/nihal1.jpg"/>
+						</div>	
+
+						
+				</div>
+			</div>	
+
+			<div class="row" style="">
+
+				<div class="col-md-12" style=" margin: -180px 0px 80px 0px;">
+					<div style="padding: 0px 20px;">
+					<div id="box3">
+						<h2 class="text1">BUY BIKE</h2><p style="color: white; font-size: 15px; margin-top: -35px; margin-left: 220px;; text-align: center;">.com</p>
+						<p class="text22">This is the first bike selling website in Bangladesh.<br>We sell bikes from different bike companies.We are adding more and more brands and bikes in our website.You can just simply sign in and order bikes!<br><br>Thanks for visiting our website and keep supporting us.<br>For any complain or query please send us a messeage from the contact us tab. </p>
+					</div>
+					</div>
+					
+					
+				</div>
+				
+
+
+			</div>		
+		</div>
+				
+
+
+
+	</div>
+
+		<?php include 'footer.php';?>
+
+        
+
+		
+	</body>
+</html>
